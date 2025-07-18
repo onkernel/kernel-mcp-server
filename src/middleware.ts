a1cc81a708a6d3a306f66ea3279e1eea6d975cd1
Mason Williams <mason@onkernel.com>
2025-07-18 09:45:51 -0400

    Add favicon.svg to public routes in middleware for accessibility

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // Public routes that don't require authentication
 const isPublicRoute = createRouteMatcher([
   "/",
+  "/favicon.svg",
   "/(.well-known)(.*)",
   "/register",
   "/authorize",
